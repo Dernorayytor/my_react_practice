c66690742a25387130ca1dc9cb43c5c36ddeb072
Dernorayytor <thenet21018@gmail.com>
2024-07-12 01:20:57 +0700

    //เพิ่มและลบสมาชิกใน Array (ด้วย pushเพิ่ม popลบ ด้านหลัง และ shiftลบ unshiftเพิ่ม ด้านหน้า)

---
+++ ทบทวน JavaScript ES6/เพิ่มและลบสมาชิกใน Array.js
@@ -0,0 +1,22 @@
+//เพิ่มและลบสมาชิกใน Array 
+//push , pop ด้านหลัง 
+// shift , unshift ด้านหน้า
+// ฟังชั่นทำงานในอาเร
+
+
+//push ใส่เข้าไป
+const data =[10,20,30]
+// console.log(data)// [10, 20, 30]
+
+// data.push(500)
+// console.log(data) // [10, 20, 30, 500]
+
+data.push(...[500,1000,2000])
+console.log(data)
+
+//pop ลบสมาชิกหลังสุด
+data.pop()
+data.pop()
+data.shift()
+data.unshift(999)
+console.log(data)
