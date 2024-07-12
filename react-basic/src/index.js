80f9a5d0f1c8e1cdd3c570f92964c991de54944e
Dernorayytor <thenet21018@gmail.com>
2024-07-12 17:51:52 +0700

    ReactDOM (การเขียน ReactDOM.render ในหน้า index.js เพื่อทำงานภายใต้ Virtual DOM)

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class HelloComponent extends React.Component{
+  render(){
+    return <h1> สวัสดี React</h1>
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <HelloComponent/>
+); 
+//ReactDOM (การเขียน ReactDOM.render ในหน้า index.js เพื่อทำงานภายใต้ Virtual DOM)
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
