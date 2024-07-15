7022487fb664b6d9d609bd0627a696a390692977
Dernorayytor <thenet21018@gmail.com>
2024-07-16 03:43:17 +0700

    Props & Spread Operator  (ใช้ Spread Operator(...) ในการลดรูปโค้ดให้สั้นลงเมื่อเรากำหนด props ไว้อยู่แล้ว)

--- react-basic/src/component/Transaction.js
+++ react-basic/src/component/Transaction.js
@@ -10,7 +10,9 @@ const Transaction =()=>{
         <ul className="item-list">
             {/* แบบloopไม่ต้องมาคอยกำหนดเวลาเพิ่มข้อมูล*/}
             {data.map((element)=>{
-                return <Item title={element.title} amount={element.amount}/>
+                return <Item {...element}/> //แบบใช้Spread Operator ในการลดรูป
+                
+                // <Item title={element.title} amount={element.amount}/> //แบบปกติ
 })} 
 
             {/* <Item title={data[0].title} amount={data[0].amount}/>
