427f6a2a8c9ee0480b82e87eeb1a6149a7bd3313
Dernorayytor <thenet21018@gmail.com>
2024-07-16 06:06:40 +0700

    Props Types (การกำหนดหรือประกาศรูปแบบของprops)

--- react-basic/src/component/Item.js
+++ react-basic/src/component/Item.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "../style/item.css"
+import PropTypes from 'prop-types'; // ES6
+
 const Item =({title,amount})=>{
     // const {title,amount}= props //ไม่ต้องมา props.amount เก็บไว้ เพื่อเรียกใช้ amount
     return (
@@ -11,6 +13,10 @@ const Item2 =()=>{
     return (
         <li className='item'>เงินเดือน<span>+20000</span></li>
 );
+}
+Item.prototype={
+    title:PropTypes.string.isRequired,
+    amount:PropTypes.number.isRequired
 }
 
 export { Item, Item2 };
