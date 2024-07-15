ad895f3aec754c5a066c3572cf24bf7844fa1b91
Dernorayytor <thenet21018@gmail.com>
2024-07-15 15:49:23 +0700

    Nested Component (การสร้างComponentในComponentเพื่อให้ดึงComponentมาใช้เพื่อให้ทำให้จัดการข้อมูลง่าย)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
+const Title =()=><h1>โปรแกรมบัญชีรายรับ รายจ่าย</h1>;
+const Description =()=><p>บันทึกข้อมูลบัญชีในแต่ละวันๆ</p>;
+const Item =()=><li>ค่าเดินทาง<spa>-200</spa></li>
+const Item2 =()=><li>เงินเดือน<spa>+20000</spa></li>
+const Transaction =()=>{
+  return (  
+      <ul>
+          <Item/>
+          <Item2/>
+          <Item/>
+      </ul>
+  );
+}
 function App() {
   return ( 
     <div>
-      <h1>โปรแกรมบัญชีรายรับ รายจ่าย</h1> 
-      <p>บันทึกข้อมูลบัญชีในแต่ละวันๆ</p>
-      <ul>
-          <li>ค่าเดินทาง<spa>-200</spa></li>
-          <li>เงินเดือน<spa>+20000</spa></li>
-          <li>ค่าข้าว<spa>-500</spa></li>
-      </ul>
+      <Title/>
+      <Description/>
+      <Transaction/>
+       
+      
+      
     </div> //ถ้าไม่มี div หรือ root elementที่มาครอบ ส่วน html ที่แทรกใน retune จะเออเร่อ จะใช้ section/article ก็ได้หรือตัวไหนก็ได้ได้หมด <> ก็ได้
     
     // <div className="App">
