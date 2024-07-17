0a538ea0c84fc534d7a798019d7a12a3e362e158
Dernorayytor <thenet21018@gmail.com>
2024-07-17 15:24:56 +0700

    Context API (Global State) (กำหนดDataContext)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Transaction from './component/Transaction';
 import FormComponent from './component/FromComponent';
 import { useState } from 'react';
+import DataContext from './data/DataContext';
 
 const Title =()=><h1>โปรแกรมบัญชีรายรับ รายจ่าย</h1>;
 
@@ -15,14 +16,13 @@ function App() {
       })
   }
   return ( 
-    <div className='container'>
-      <Title/>
-      <FormComponent onAddItem = {onAddNewItem}/> {/*สร้างฟังก์ชั่นเพื่อรอรับค้าที่จะส่งมาจาก FromComponent.js*/}
-      <Transaction items = {items}/>
-       
-      
-      
-    </div> //ถ้าไม่มี div หรือ root elementที่มาครอบ ส่วน html ที่แทรกใน retune จะเออเร่อ จะใช้ section/article ก็ได้หรือตัวไหนก็ได้ได้หมด <> ก็ได้
+    <DataContext>
+      <div className='container'>
+        <Title/>
+        <FormComponent onAddItem = {onAddNewItem}/> {/*สร้างฟังก์ชั่นเพื่อรอรับค้าที่จะส่งมาจาก FromComponent.js*/}
+        <Transaction items = {items}/>
+      </div> {/*ถ้าไม่มี div หรือ root elementที่มาครอบ ส่วน html ที่แทรกใน retune จะเออเร่อ จะใช้ section/article ก็ได้หรือตัวไหนก็ได้ได้หมด <> ก็ได้*/}
+    </DataContext>
      
   );
 }
