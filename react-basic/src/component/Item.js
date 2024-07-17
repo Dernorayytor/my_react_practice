ef33d708f542f1262e96e1b2fae25f8da8f00832
Dernorayytor <thenet21018@gmail.com>
2024-07-17 12:02:05 +0700

    Item Style (เพิ่มการกำหนดsymbolหน้ารายการ)

--- react-basic/src/component/Item.js
+++ react-basic/src/component/Item.js
@@ -5,8 +5,9 @@ import PropTypes from 'prop-types'; // ES6
 const Item =(props)=>{
     const {title,amount}= props //ไม่ต้องมา props.amount เก็บไว้ เพื่อเรียกใช้ amount
     const status = amount<0 ? 'expense' : 'income'
+    const symbol = amount<0 ? '-' : '+' //หรือ amount<0 ? '' : '+' ถ้าไม่ใช้ Math.abs(amount)
     return (
-        <li className={status}>{title}<span>{amount}</span></li>
+        <li className={status}>{title}<span>{symbol}{Math.abs(amount)}</span></li>
 );
 }
 
