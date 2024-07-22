0aa4dcb3efaf42292e84602fa039d77f47d14a1e
Dernorayytor <thenet21018@gmail.com>
2024-07-22 18:25:59 +0700

    useReducer (การเพิ่มลดเลขด้วย reducer state และกำหนด payload)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,8 +1,7 @@
 import './style/App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect,useState ,useReducer} from 'react';
 import Transaction from './component/Transaction';
 import FormComponent from './component/FromComponent';
-import { useState } from 'react';
 import DataContext from './data/DataContext';
 import ReportComponent from './component/ReportComponent';
 import { element } from 'prop-types';
@@ -29,21 +28,40 @@ function App() {
     setReportExpense(expense)
     setReportIncome(income)
   },[items,ReportIncome,ReportExpense]) //ทุกครั้งที่ items มีการเปลี่ยนแปลงให้ทำงานใน useEffect ด้านล่า��
+  
+  //reducer state
+  const [count,setCount] = useState(0)
+  const reducer = (state,action)=>{
+        switch(action.type){
+          case 'ADD' :
+            return state+action.payload
+          case 'SUB' :
+            return state-action.payload
+          case 'CLEAR' :
+              return 0
+        }
+  }
+  const [result,dispatch] = useReducer(reducer,count)
   return ( 
-    <DataContext.Provider value={
-      {
-        income: ReportIncome,
-        expense:ReportExpense
-      }
-    }>  {/*เป็นข้อมูลกลางที่สามารถดึงไปใช้ได้กัน*/}
-      <div className='container'>
-        <Title/>
-        <ReportComponent/>
-        <FormComponent onAddItem = {onAddNewItem}/> {/*สร้างฟังก์ชั่นเพื่อรอรับค้าที่จะส่งมาจาก FromComponent.js*/}
-        <Transaction items = {items}/>
-      </div> {/*ถ้าไม่มี div หรือ root elementที่มาครอบ ส่วน html ที่แทรกใน retune จะเออเร่อ จะใช้ section/article ก็ได้หรือตัวไหนก็ได้ได้หมด <> ก็ได้*/}
-    </DataContext.Provider>
-     
+    // <DataContext.Provider value={
+    //   {
+    //     income: ReportIncome,
+    //     expense:ReportExpense
+    //   }
+    // }>  {/*เป็นข้อมูลกลางที่สามารถดึงไปใช้ได้กัน*/}
+    //   <div className='container'>
+    //     <Title/>
+    //     <ReportComponent/>
+    //     <FormComponent onAddItem = {onAddNewItem}/> {/*สร้างฟังก์ชั่นเพื่อรอรับค้าที่จะส่งมาจาก FromComponent.js*/}
+    //     <Transaction items = {items}/>
+    //   </div> {/*ถ้าไม่มี div หรือ root elementที่มาครอบ ส่วน html ที่แทรกใน retune จะเออเร่อ จะใช้ section/article ก็ได้หรือตัวไหนก็ได้ได้หมด <> ก็ได้*/}
+    // </DataContext.Provider>
+     <div align = "center">
+      <h1>{result}</h1>
+      <button onClick={()=>dispatch({type:"ADD",payload:10})}>เพิ่ม</button>
+      <button onClick={()=>dispatch({type:"SUB",payload:5})}>ลด</button>
+      <button onClick={()=>dispatch({type:"CLEAR"})}>ล้าง</button>
+     </div>
   );
 }
 
